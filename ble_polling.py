fe2154c7cc4702ca7f8a904c2da87260e7ebea07
Anders Fyhn <githib@fyhn.se>
2017-03-12 21:12:51 +0000

    Added list of my_microbits

--- ble_polling.py
+++ ble_polling.py
@@ -1,42 +1,55 @@
 import dbus
 import sys
 from time import sleep
+import re
 
-name = sys.argv[1]
+#name = sys.argv[1]
 bus = dbus.SystemBus()
 bluez = bus.get_object('org.bluez','/')
 bluez_iface = dbus.Interface(bluez, 'org.freedesktop.DBus.ObjectManager')
 objects = bluez_iface.GetManagedObjects()
 
-for obj, ifaces in objects.items():
-    if 'org.bluez.Device1' in ifaces.keys():
-        if 'Name' in ifaces['org.bluez.Device1']:
-            if name in ifaces['org.bluez.Device1']['Name']:
-                print "Name match with", name
-                device_path = obj
+my_microbits = ['zotev']
+remaining_microbits = list(my_microbits)
 
-print device_path
+device_paths=dict()
 
-remote_device_obj = bus.get_object('org.bluez', device_path)
-remote_device_methods = dbus.Interface(remote_device_obj, 'org.bluez.Device1')
-remote_device_props = dbus.Interface(remote_device_obj, dbus.PROPERTIES_IFACE)
+while len(remaining_microbits)>0:
+    for obj, ifaces in objects.items():
+        if 'org.bluez.Device1' in ifaces.keys():
+            if 'Name' in ifaces['org.bluez.Device1']:
+                m=re.search('BBC micro:bit \[(\w+)\]', ifaces['org.bluez.Device1']['Name'])
+                if m:
+                    name=m.group(1)
+                    print "Trying", name
+                    if name in my_microbits:
+                        print "Matched", name
+                        device_paths[name] = obj
+                        remaining_microbits.remove(name)
 
-remote_device_methods.Connect()
+print device_paths
 
-while not remote_device_props.Get('org.bluez.Device1','ServicesResolved'):
-    sleep(0.25)
+for name, device_path in device_paths.iteritems():
+    remote_device_obj = bus.get_object('org.bluez', device_path)
+    remote_device_methods = dbus.Interface(remote_device_obj, 'org.bluez.Device1')
+    remote_device_props = dbus.Interface(remote_device_obj, dbus.PROPERTIES_IFACE)
 
-for obj, ifaces in objects.items():
-    if 'org.bluez.GattCharacteristic1' in ifaces.keys():
-        if obj.startswith(device_path):
-            if ifaces['org.bluez.GattCharacteristic1']['UUID'] == 'e95dda90-251d-470a-a062-fa1922dfa9a8':
-                btn_a_path = obj
-            if ifaces['org.bluez.GattCharacteristic1']['UUID'] == 'e95dda91-251d-470a-a062-fa1922dfa9a8':
-                btn_b_path = obj
-            if ifaces['org.bluez.GattCharacteristic1']['UUID'] == 'e95d93ee-251d-470a-a062-fa1922dfa9a8':
-                led_path = obj
-            if ifaces['org.bluez.GattCharacteristic1']['UUID'] == '6e400003-b5a3-f393-e0a9-e50e24dcca9e':
-                uart_path = obj
+    remote_device_methods.Connect()
+
+    while not remote_device_props.Get('org.bluez.Device1','ServicesResolved'):
+        sleep(0.25)
+
+    for obj, ifaces in objects.items():
+        if 'org.bluez.GattCharacteristic1' in ifaces.keys():
+            if obj.startswith(device_path):
+                if ifaces['org.bluez.GattCharacteristic1']['UUID'] == 'e95dda90-251d-470a-a062-fa1922dfa9a8':
+                    btn_a_path = obj
+                if ifaces['org.bluez.GattCharacteristic1']['UUID'] == 'e95dda91-251d-470a-a062-fa1922dfa9a8':
+                    btn_b_path = obj
+                if ifaces['org.bluez.GattCharacteristic1']['UUID'] == 'e95d93ee-251d-470a-a062-fa1922dfa9a8':
+                    led_path = obj
+                if ifaces['org.bluez.GattCharacteristic1']['UUID'] == '6e400003-b5a3-f393-e0a9-e50e24dcca9e':
+                    uart_path = obj
 
                 
 print btn_a_path
